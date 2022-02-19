db1b906b5a562605f89b0eb14ad92ecbc1ef5479
Kamsu10 <kamsudylane@gmail.com>
2022-02-19 18:48:31 +0100

    first commit

---
+++ src/pages/Student.js
@@ -0,0 +1,15 @@
+import React, { Component} from "react";
+
+class Student extends Component
+{
+   render() {
+    return (
+    <div className='container'>
+        
+    </div>
+    )
+   }
+}
+
+
+export default Student;
