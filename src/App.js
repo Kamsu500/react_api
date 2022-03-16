823f9d6f3211f8e9e7b2358c4f92117bc000b1f7
Kamsu10 <kamsudylane@gmail.com>
2022-03-16 03:32:19 +0100

    recent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React,{useState,useEffect} from 'react';
 import './App.css';
+import Navbar from './components/navbar/navbar';
+import { getAllPokemon } from './services/pokemon'
+
 
 function App() {
-  return (
-    <div className='container'>
-        bonjour
-    </div>
-  );
-}
+    const [pokemonData,setPokemonData]=useState([]);
+    const [nextUrl,setNextUrl]=useState('');
+    const [prevUrl,setPrevUrl]=useState('');
+    const [loading,setLoading]=useState(true);
+    const initialUrl='https://pokeapi.co/api/v2/pokemon'
 
+    useEffect(()=>{
+      async function fetchData(){
+        let response = await getAllPokemon(initialUrl);
+        console.log(response);
+        setNextUrl(response.next);
+        setPrevUrl(response.previous);
+        setLoading(false);
+      }
+      fetchData();
+    },[]);
+
+    return <div>
+            <Navbar/>
+           {loading ? <h1>Loading ...</h1> : <h1>Data fetched successfully</h1>}
+    
+           </div>;
+}
 export default App;
